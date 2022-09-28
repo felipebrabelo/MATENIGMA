7203d03732075e3769a6f451eca6bca8aeb6b724
GuilhermeWisniewski <114250592+GuilhermeWisniewski@users.noreply.github.com>
2022-09-28 15:03:30 -0300

    Add files via upload

--- screen_name.c
+++ screen_name.c
@@ -44,35 +44,22 @@ FILE* fopen_e_teste(char* caminho, char* modo) {
 }
 
 int compara(const void* a, const void* b) {
-    player* n1 = (player*) a;
-    player* n2 = (player*) b;
-    return (n2->score - n1->score);
+    int* n1 = (int*)a;
+    int* n2 = (int*)b;
+    return -(* n1 - *n2);
 }
 
-void criaranking(player* list) {
-
-    int i;
-    FILE* pf = fopen_e_teste(NOME_ARQUIVO, "wb+");
-
-    qsort(list, 11, sizeof(player), compara);
-
-    for (i = 0; i < 10; i++) {
-        fwrite(&list[i], sizeof(player), 1, pf);
-    }
-    fclose(pf);
-}
-
-int seleciona(){
+int seleciona() {
     //player* list;
     player teste;
     double x;
     int i;
-    int cont = 1;
+    int cont = 0;
     int zerado = 0;
 
-    FILE* fp = fopen_e_teste(NOME_ARQUIVO, "rb");
-    FILE* scoreatual = fopen_e_teste(PONTOS_ATUAL, "rb");
-    
+    FILE* fp = fopen_e_teste(NOME_ARQUIVO, "rb+");
+    FILE* scoreatual = fopen_e_teste(PONTOS_ATUAL, "rb+");
+
     list = (player*)malloc(11 * sizeof(player));
     if (list == NULL) {
         perror("Erro ao alocar. \n");
@@ -81,20 +68,27 @@ int seleciona(){
     fread(&x, sizeof(double), 1, scoreatual);
 
     for (i = 0; i < 11; i++) {
-        list[i].score = 0;
+        list[i].score = 2;
     }
-    //list[10].score = x;
+    list[10].score = x;
 
-    while (fread(&teste, sizeof(player), 1, fp) > 0 && zerado<10) {
-        list[zerado] = teste;
+    while (fread(&teste, sizeof(player), 1, fp) > 0 && zerado < 10) {
+        list[zerado].score = teste.score;
+        strcpy(list[zerado].name, teste.name);
         if (list[10].score > list[zerado].score) {
             cont++;
         }
         zerado++;
     }
-    if (cont || !zerado) {
-        criaranking(list);
+
+    qsort(list, 11, sizeof(player), compara);
+
+    rewind(fp);
+
+    for (i = 0; i < 10 && list[i].score > 0; i++) {
+        fwrite(&list[i], sizeof(player), 1, fp);
     }
+
     fclose(fp);
     fclose(scoreatual);
     return (cont || !zerado);
@@ -123,83 +117,86 @@ void DrawNameScreen(void)
 
     Rectangle textBox = { screenWidth / 2.0f - 100, screenHeight / 2 - 25, 500, 60 };
     bool mouseOnText = false;
-    seleciona();
     // Update
     //----------------------------------------------------------------------------------
-    /*if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
-    else mouseOnText = false;
+    if (seleciona()) {
+        if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
+        else mouseOnText = false;
 
-    if (mouseOnText)
-    {
-        // Set the window's cursor to the I-Beam
-        SetMouseCursor(MOUSE_CURSOR_IBEAM);
-
-        // Get char pressed (unicode character) on the queue
-        int key = GetCharPressed();
-
-        // Check if more characters have been pressed on the same frame
-        while (key > 0)
+        if (mouseOnText)
         {
-            // NOTE: Only allow keys in range [32..125]
-            if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
+            // Set the window's cursor to the I-Beam
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+
+            // Get char pressed (unicode character) on the queue
+            int key = GetCharPressed();
+
+            // Check if more characters have been pressed on the same frame
+            while (key > 0)
             {
-                name[letterCount] = (char)key;
-                name[letterCount + 1] = '\0'; // Add null terminator at the end of the string.
-                letterCount++;
+                // NOTE: Only allow keys in range [32..125]
+                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
+                {
+                    name[letterCount] = (char)key;
+                    name[letterCount + 1] = '\0'; // Add null terminator at the end of the string.
+                    letterCount++;
+                }
+
+                key = GetCharPressed();  // Check next character in the queue
             }
 
-            key = GetCharPressed();  // Check next character in the queue
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                letterCount--;
+                if (letterCount < 0) letterCount = 0;
+                name[letterCount] = '\0';
+            }
         }
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 
-        if (IsKeyPressed(KEY_BACKSPACE))
+        if (mouseOnText) framesCounter++;
+        else framesCounter = 0;
+
+        // Draw
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("FIM DE JOGO", screenWidth / 2 - 200, screenHeight / 2 - 200, 50, BLACK);
+        DrawText(TextFormat("Sua pontuacao foi de: x pontos"), screenWidth / 2 - 300, screenHeight / 2 - 100, 40, GRAY);
+        DrawRectangleRec(textBox, LIGHTGRAY);
+        if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
+        else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
+
+        DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
+
+        DrawText(TextFormat("Digite seu Nome: %i/%i", letterCount, MAX_INPUT_CHARS), screenWidth / 2 - 500, screenHeight / 2, 30, DARKGRAY);
+
+        if (mouseOnText)
         {
-            letterCount--;
-            if (letterCount < 0) letterCount = 0;
-            name[letterCount] = '\0';
+            if (letterCount < MAX_INPUT_CHARS)
+            {
+                // Draw blinking underscore char
+                if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(name, 40), (int)textBox.y + 12, 40, MAROON);
+            }
+            else DrawText("Press BACKSPACE to delete chars...", 230, 300, 20, GRAY);
         }
+        strcpy(list[10].name, name);
     }
-    else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
-
-    if (mouseOnText) framesCounter++;
-    else framesCounter = 0;
-
-    // Draw
-
-    ClearBackground(RAYWHITE);
-
-    DrawText("FIM DE JOGO", screenWidth / 2 - 200, screenHeight / 2 - 200, 50, BLACK);
-    DrawText(TextFormat("Sua pontuacao foi de: x pontos"), screenWidth / 2 - 300, screenHeight / 2 - 100, 40, GRAY);
-    DrawRectangleRec(textBox, LIGHTGRAY);
-    if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
-    else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
-
-    DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
-
-    DrawText(TextFormat("Digite seu Nome: %i/%i", letterCount, MAX_INPUT_CHARS), screenWidth / 2 - 500, screenHeight / 2, 30, DARKGRAY);
-
-    if (mouseOnText)
-    {
-        if (letterCount < MAX_INPUT_CHARS)
-        {
-            // Draw blinking underscore char
-            if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(name, 40), (int)textBox.y + 12, 40, MAROON);
-        }
-        else DrawText("Press BACKSPACE to delete chars...", 230, 300, 20, GRAY);
-    }
-    strcpy(list[10].name, name);*/
-
 
     DrawText(TextFormat("rank   nome                  pontos \n"), screenWidth / 2 + 100, screenHeight / 2 + 100, 25, GRAY);
     DrawText(TextFormat("rank   nome                  pontos \n"), screenWidth / 2 - 500, screenHeight / 2 + 100, 25, GRAY);
-    DrawText(TextFormat(" %d    %s\n", 0 + 1, list[0].name), screenWidth / 2 - 300, screenHeight / 2 + 50, 15, GRAY);
-    DrawText(TextFormat("                             %lf\n", list[0].score), screenWidth / 2 - 300, screenHeight / 2 + 50, 15, GRAY);
-    /*for (int i = 0; i < 5; i++) {
-        DrawText(TextFormat(" %d    %s\n", i + 1, list[i].name), screenWidth / 2 - 300, screenHeight / 2 + 50, 15, GRAY);
-    }
-    for (int i = 0; i < 5; i++) {
-        DrawText(TextFormat("                             %lf\n", list[i].score), screenWidth / 2 - 300, screenHeight / 2 + 50, 15, GRAY);
-    }
-    for (int i = 5; i < 10; i++) {
+
+        DrawText(TextFormat("-!-!-_!-!%s\n",list[5].name), screenWidth / 2 - 300, screenHeight / 2 + 50, 40, BLACK);
+        DrawText(TextFormat("---%s\n", list[0].name), screenWidth / 2 - 300, screenHeight / 2 + 80, 15, BLACK);
+        DrawText(TextFormat("---%s\n", list[1].name), screenWidth / 2 - 300, screenHeight / 2 + 100, 15, BLACK);
+        DrawText(TextFormat("---%s\n", list[2].name), screenWidth / 2 - 300, screenHeight / 2 + 120, 15, BLACK);
+        DrawText(TextFormat("---%s\n", list[3].name), screenWidth / 2 - 300, screenHeight / 2 + 140, 15, BLACK);
+        DrawText(TextFormat("---%s\n", list[4].name), screenWidth / 2 - 300, screenHeight / 2 + 160, 15, BLACK);
+
+
+        DrawText(TextFormat("---%lf\n", list[5].score), screenWidth / 2 - 300, screenHeight / 2 + 80, 15, BLACK);
+        DrawText(TextFormat("                             %lf\n", list[0].score), screenWidth / 2 - 300, screenHeight / 2 + 50, 15, GRAY);
+    /*for (int i = 5; i < 10; i++) {
         DrawText(TextFormat(" %d    %s\n", i + 1, list[i].name), screenWidth / 2 + 300, screenHeight / 2 + 50, 15, GRAY);
     }
     for (int i = 5; i < 10; i++) {
